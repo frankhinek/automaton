8608064c10348fdec662db639c52fbed8e2823ef
Frank Hinek <frank.hinek@gmail.com>
2018-02-25 00:57:37 -0500

    Dotfiles is working

---
+++ roles/dotfiles/files/pyenv/init.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
---
+++ roles/dotfiles/files/zsh/path.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+SYSTEM_PATH=$PATH
+unset PATH
+
+# keep these on separate lines to make changing their order easier
+PATH=$SYSTEM_PATH
+export PATH
