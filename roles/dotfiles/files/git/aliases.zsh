b258aa3b830606ad8664d9ffbc42a2f1d4d05043
Frank Hinek <frank.hinek@gmail.com>
2018-02-25 02:58:59 -0500

    Add git aliases

---
+++ roles/dotfiles/files/git/aliases.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+alias gl='git pull --prune'
+alias glg="git log --graph --decorate --oneline --abbrev-commit"
+alias glga="glg --all"
+alias gp='git push origin HEAD'
+alias gpa='git push origin --all'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gcf='git commit --fixup'
+alias gco='git checkout'
+alias gb='git branch'
+alias ga='git add'
+alias gaa='git add -A'
+alias gcm='git commit -m'
+alias gcam='git commit -a -m'
+alias gs='git status -sb'
+alias gcl='git clone'
+
+gi() {
+  curl -s "https://www.gitignore.io/api/$*";
+}
