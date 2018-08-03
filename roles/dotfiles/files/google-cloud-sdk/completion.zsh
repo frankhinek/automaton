36130e6004c0b267e3b7d032547aa53468e113b0
Frank Hinek <accounts@hinek.com>
2018-08-03 12:41:42 -0400

    Add Google Cloud SDK to homebrew and dotfiles roles

---
+++ roles/dotfiles/files/google-cloud-sdk/completion.zsh
@@ -0,0 +1,2 @@
+#!/usr/bin/env zsh
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
---
+++ roles/dotfiles/files/google-cloud-sdk/path.zsh
@@ -0,0 +1,2 @@
+#!/usr/bin/env zsh
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
