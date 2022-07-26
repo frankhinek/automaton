17ab0950209ef278b9647b0c209d3beb867594f3
Frank Hinek <frankhinek@users.noreply.github.com>
2022-07-26 10:15:32 -0400

    Update Ansible dynamic inventory script to work with Ansible v2 and onward

--- inventory/dynamic.py
+++ inventory/dynamic.py
@@ -1,28 +1,71 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+
+'''
+Custom dynamic inventory script for Ansible, in Python.
+'''
+
+import os
+import sys
+import argparse
+import json
 
 import re
 import socket
 
-host = socket.gethostname()
+class ExampleInventory(object):
 
-if host == 'WUSFH185036-L8Q':
-    group = 'work'
-elif re.match(r'fdh-mbp(?:\.(?:local|lan)?)?\Z', host):
-    group = 'personal'
-elif re.match(r'dev(?:vm)?\d+', host):
-    group = 'devservers'
-else:
-    group = 'local'
+    def __init__(self):
+        self.inventory = {}
+        self.read_cli_args()
 
-print """
-{
-  "%s": {
-    "hosts": [
-      "localhost"
-    ],
-    "vars": {
-      "ansible_connection": "local"
-    }
-  }
-}
-""" % group
+        # Called with `--list`.
+        if self.args.list:
+            self.inventory = self.example_inventory()
+        # Called with `--host [hostname]`.
+        elif self.args.host:
+            # Not implemented, since we return _meta info `--list`.
+            self.inventory = self.empty_inventory()
+        # If no groups or vars are present, return empty inventory.
+        else:
+            self.inventory = self.empty_inventory()
+
+        print(json.dumps(self.inventory));
+
+    # Example inventory for testing.
+    def example_inventory(self):
+        host = socket.gethostname()
+
+        if host == 'WUSFH185036-UC1':
+            group = 'work'
+        elif re.match(r'fdh-mbp(?:\.(?:local|lan)?)?\Z', host):
+            group = 'personal'
+        elif re.match(r'dev(?:vm)?\d+', host):
+            group = 'devservers'
+        else:
+            group = 'local'
+
+        return {
+            group: {
+                'hosts': ['localhost'],
+                'vars': {
+                    'ansible_connection': 'local'
+                }
+            },
+            '_meta': {
+                'hostvars': {}
+            }
+        }
+
+    # Empty inventory for testing.
+    def empty_inventory(self):
+        return {'_meta': {'hostvars': {}}}
+
+    # Read the command line args passed to the script.
+    def read_cli_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--list', action = 'store_true')
+        parser.add_argument('--host', action = 'store')
+        self.args = parser.parse_args()
+
+# Get the inventory.
+ExampleInventory()
