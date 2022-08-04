dd77228945b3e7bed1f709b4e85ac1637bdd5c18
Frank Hinek <frankhinek@users.noreply.github.com>
2022-08-03 20:26:47 -0400

    Add fdfind and bat
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- roles/dotfiles/files/system/install.fish
+++ roles/dotfiles/files/system/install.fish
@@ -10,6 +10,17 @@ else
 	abbr -a ll 'ls -l'
 end
 
+if command -qs fdfind
+	alias --save fd=fdfind
+end
+
+if command -qa bat
+	mkdir -p "$(bat --config-dir)"
+	ln -sf $DOTFILES/system/bat.config $HOME/.config/bat/config
+	alias --save cat=bat
+	set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
+end
+
 if command -qs zoxide
 	zoxide init fish >$__fish_config_dir/conf.d/zoxide.fish
 end
