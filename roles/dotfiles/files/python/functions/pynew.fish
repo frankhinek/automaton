cea80434a6325522b4a441eb1c27df63c7257ae5
Frank Hinek <frankhinek@users.noreply.github.com>
2022-10-27 09:54:04 -0400

    Improve fish pynew function reliability
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- roles/dotfiles/files/python/functions/pynew.fish
+++ roles/dotfiles/files/python/functions/pynew.fish
@@ -1,9 +1,15 @@
 function pynew -d "create a new Python project"
   poetry new $argv[1]
     and cd (basename $argv[1] .git)
+  wait
   git init -b main
+  wait
   gi python
+  wait
   poetry shell
+  wait
   poetry add -G dev pytest mkdocs-material black
+  wait
   mkdocs new .
+  wait
 end
