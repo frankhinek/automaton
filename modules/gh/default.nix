c0f35ee3c10a9396cd7b3da7575477df2079becf
Frank Hinek <frankhinek@users.noreply.github.com>
2024-12-26 08:12:50 -0500

    Add GitHub CLI tool
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

---
+++ modules/gh/default.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+{
+  programs.gh = {
+    enable = true;
+    settings = {
+      # Workaround for https://github.com/nix-community/home-manager/issues/4744
+      version = 1;
+      aliases = {
+        clone = "repo clone";
+        co = "pr checkout";
+        v = "repo view --web";
+        pv = "pr view --web";
+        pr = "pr create --web";
+      };
+      editor = "nvim";
+      git_protocol = "ssh";
+    };
+  };
+}
