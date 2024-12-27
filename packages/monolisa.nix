37d41d621f3344333dc3395fb1a1ba5ddb5cd4a7
Frank Hinek <frankhinek@users.noreply.github.com>
2024-12-27 11:23:24 -0500

    Fix private repo bug
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/monolisa.nix
+++ packages/monolisa.nix
@@ -6,12 +6,16 @@
 stdenvNoCC.mkDerivation rec {
   pname = "monolisa-fonts";
   version = "v2.000";
-  src = fetchFromGitHub {
+  src = (fetchFromGitHub {
     owner = "frankhinek";
     repo = "fonts-licensed";
     rev = "main";
     sha256 = "11s01ysxg3777zvg85zb7snpfymr7zg8276vb7923nzvrvqq25a9";
-  };
+  }).overrideAttrs (_: {
+    GIT_CONFIG_COUNT = 1;
+    GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
+    GIT_CONFIG_VALUE_0 = "ssh+git@github.com:";
+  });
 
   buildPhase = "true"; # No build needed
 
