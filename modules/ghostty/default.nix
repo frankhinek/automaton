c7a9a2a1fb591ffbc68900f9f60d30f63c33e23b
Frank Hinek <frankhinek@users.noreply.github.com>
2024-12-26 19:23:39 -0500

    Try to install ghostty
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- modules/ghostty/default.nix
+++ modules/ghostty/default.nix
@@ -1,5 +1,9 @@
 { config, ... }:
 {
+  home.packages = [
+    ghostty.packages.aarch64-darwin.default
+  ];
+
   xdg.configFile."ghostty/config" = {
     source = config.lib.file.mkOutOfStoreSymlink ./config;
   };
