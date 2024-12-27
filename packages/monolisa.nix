0b1d4506557419642d6ac6bf242595af83a8ff8e
Frank Hinek <frankhinek@users.noreply.github.com>
2024-12-27 16:38:15 -0500

    Try to change directory
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/monolisa.nix
+++ packages/monolisa.nix
@@ -5,13 +5,13 @@
 stdenvNoCC.mkDerivation {
   pname = "monolisa-nerdfonts";
   version = "v2.000";
-  src = ./.;
+  src = ./fonts;
 
   buildPhase = "true"; # No build needed
 
   installPhase = ''
     mkdir -p $out/share/fonts/truetype
-    cp "$src/fonts/MonoLisa-Plus/$version/ttf-nerd-font/"*.ttf $out/share/fonts/truetype/
+    cp $src/MonoLisa-Plus/$version/ttf-nerd-font/*.ttf $out/share/fonts/truetype/
   '';
 
   # Adding meta information
