042c953c4376e922c5464a5b669e5a32ad268e35
Frank Hinek <frankhinek@users.noreply.github.com>
2024-12-26 19:39:03 -0500

    Try to fix ghostty install
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -76,6 +76,11 @@
           system = "aarch64-darwin";
           modules = [
             { nixpkgs.overlays = overlays; }
+            {
+              environment.systemPackages = [
+                ghostty.packages.aarch64-darwin.default
+              ];
+            }
             ./machines/wintermute
             home-manager.darwinModules.home-manager
             {
--- modules/ghostty/default.nix
+++ modules/ghostty/default.nix
@@ -1,9 +1,5 @@
-{ config, ghostty, ... }:
+{ config, ... }:
 {
-  home.packages = [
-    ghostty.packages.aarch64-darwin.default
-  ];
-
   xdg.configFile."ghostty/config" = {
     source = config.lib.file.mkOutOfStoreSymlink ./config;
   };
