38ac5eb71915fbf601d7990178ff8d475313892a
Frank Hinek <frankhinek@users.noreply.github.com>
2024-12-28 11:29:48 -0500

    Test again
    Signed-off-by: Frank Hinek <frankhinek@users.noreply.github.com>

--- packages/monolisa.nix
+++ packages/monolisa.nix
@@ -5,7 +5,7 @@
 stdenvNoCC.mkDerivation {
   pname = "monolisa-nerdfonts";
   version = "v2.000";
-  src = ./.;
+  src = ./thing;
 
   buildPhase = "true"; # No build needed
 
@@ -13,7 +13,7 @@ stdenvNoCC.mkDerivation {
     mkdir -p $out/share/fonts/truetype
     cd $src && pwd
     ls -al $src
-    cp $src/thing/MonoLisa-Plus/$version/ttf-nerd-font/*.ttf $out/share/fonts/truetype/
+    cp $src/MonoLisa-Plus/$version/ttf-nerd-font/*.ttf $out/share/fonts/truetype/
   '';
 
   # Adding meta information
