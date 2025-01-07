aa0e0d04f1e5dab2edb8e315f3c247fe0f57be94
Frank Hinek <frankhinek@users.noreply.github.com>
2025-01-07 06:03:46 -0500

    Restore prior dev fish function

--- modules/home/programs/cli/shells/fish/functions/dev.fish
+++ modules/home/programs/cli/shells/fish/functions/dev.fish
@@ -1,25 +1,23 @@
-# function dev --description 'Wrapper for nix develop with shell shortcuts'
-# #    cd ~/.automaton
-#    if test (count $argv) -eq 0
-#        nix develop $AUTOMATON_HOME/.# -c $SHELL
-#    else
-#        nix develop $AUTOMATON_HOME/.#$argv[1] -c $SHELL
-#    end
-# #    cd -
-# end
 function dev --description 'Wrapper for nix develop with shell shortcuts'
-    set -l original_dir (pwd)
-    cd $AUTOMATON_HOME
-
     if test (count $argv) -eq 0
-        nix develop .# -c $SHELL
+        nix develop $AUTOMATON_HOME/.# -c $SHELL
     else
-        nix develop .#$argv[1] -c $SHELL
-    end
-
-    if test -d $original_dir
-        cd $original_dir
-    else
-        cd -
+        nix develop $AUTOMATON_HOME/.#$argv[1] -c $SHELL
     end
 end
+# function dev --description 'Wrapper for nix develop with shell shortcuts'
+#     set -l original_dir (pwd)
+#     cd $AUTOMATON_HOME
+
+#     if test (count $argv) -eq 0
+#         nix develop .# -c $SHELL
+#     else
+#         nix develop .#$argv[1] -c $SHELL
+#     end
+
+#     if test -d $original_dir
+#         cd $original_dir
+#     else
+#         cd -
+#     end
+# end
