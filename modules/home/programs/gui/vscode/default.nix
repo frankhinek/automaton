3394d5491d1ae03f3cd5b1bbaf51ac82081cbe48
Frank Hinek <frankhinek@users.noreply.github.com>
2025-01-12 19:42:24 -0500

    Customize VS Code settings

--- modules/home/programs/gui/vscode/default.nix
+++ modules/home/programs/gui/vscode/default.nix
@@ -10,6 +10,15 @@ let
   inherit (lib.${namespace}) mkBoolOpt;
 
   cfg = config.${namespace}.programs.gui.vscode;
+
+  # Custom marketplace extensions that are not packaged with nixpkgs.
+  # Determine sha256 hash using: nix-prefetch-url https://marketplace.visualstudio.com/_apis/public/gallery/publishers/miguelsolorio/vsextensions/fluent-icons/0.0.19/vspackage
+  fluent-icons = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "fluent-icons";
+    publisher = "miguelsolorio";
+    version = "0.0.19";
+    sha256 = "17rplc681rjpskn9h7lk02349j57vqyp7d7q76c3z9cs8j3x5wrr";
+  };
 in
 {
   options.${namespace}.programs.gui.vscode = {
@@ -25,13 +34,56 @@ in
 
       extensions = with pkgs.vscode-extensions; [
         enkia.tokyo-night
+        fluent-icons
         jnoortheen.nix-ide
+        pkief.material-icon-theme
         rust-lang.rust-analyzer
+      ];
+
+      keybindings = [
+        {
+          key = "cmd+k cmd+e";
+          command = "workbench.view.explorer";
+        }
+        {
+          key = "cmd+k cmd+f";
+          command = "workbench.view.search";
+        }
+        {
+          key = "cmd+k cmd+g";
+          command = "workbench.view.scm";
+        }
+        {
+          key = "cmd+k cmd+d";
+          command = "workbench.view.debug";
+        }
+        {
+          key = "cmd+k cmd+x";
+          command = "workbench.view.extensions";
+        }
       ];
 
       userSettings = mkIf cfg.declarativeConfig {
         # Color theme
+        "material-icon-theme.folders.theme" = "none";
         "workbench.colorTheme" = "Tokyo Night";
+        "workbench.iconTheme" = "material-icon-theme";
+
+        # Editor Appearance
+        "editor.minimap.enabled" = false;
+        "editor.rulers" = [ 80 ];
+        "workbench.editor.tabActionCloseVisibility" = false;
+        "workbench.productIconTheme" = "fluent-icons";
+        "workbench.sideBar.location" = "right";
+
+        # Typography
+        "editor.fontFamily" = "MonoLisa Nerd Font"; # (Default: Menlo, Monaco, 'Courier New', monospace)
+        "editor.fontLigatures" = "'ss02' on, 'calt' on, 'liga' on, 'zero' on"; # (Default: null)
+        "editor.fontSize" = 14; # (Default: 12)
+        "editor.lineHeight" = 0; # (Default: 0 - automatic)
+        "terminal.integrated.fontLigatures" = true; # (Default: false)
+        "terminal.integrated.fontSize" = 13; # (Default: 12)
+        "terminal.integrated.lineHeight" = 0; # (Default: 0 - automatic)
       };
     };
   };
