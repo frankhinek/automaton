9e8d45fda57b1c604f47ea29ef740aa51f42c3db
Frank Hinek <frankhinek@users.noreply.github.com>
2025-03-20 05:13:37 -0400

    fix: Add RUST_SRC_PATH to Rust dev shell

--- shells/rust/default.nix
+++ shells/rust/default.nix
@@ -6,7 +6,10 @@ mkShell {
     rust-analyzer
     rustc
     rustfmt
+    rustPlatform.rustLibSrc
   ];
+
+  RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
 
   shellHook = ''
     printf "\n    \033[1;35m🔨 Rust DevShell\033[0m\n\n"
