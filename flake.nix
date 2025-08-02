5a975bf95ee4c46dc8ebf01c8d7cd7671e0e0e7f
Frank Hinek <frankhinek@users.noreply.github.com>
2025-08-02 07:25:45 -0400

    chore: Use latest nixpkgs-unstable commit to install latest claude code

--- flake.nix
+++ flake.nix
@@ -27,7 +27,8 @@
 
     # NixPkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/de20ed5e4000fbd3b77021556b3ebe66bbc73733";
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Nix User Repository
     # nur.url = "github:nix-community/NUR";
