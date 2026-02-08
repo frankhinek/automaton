2d26402ec360ef8afd904ea2df7d0c292a1bb067
Frank Hinek <frankhinek@users.noreply.github.com>
2026-02-08 10:06:08 -0500

    chore: add Bun bin directory to system path

--- modules/darwin/programs/cli/bun/default.nix
+++ modules/darwin/programs/cli/bun/default.nix
@@ -1,23 +1,19 @@
-{
-  config,
-  lib,
-  pkgs,
-  namespace,
-  ...
-}:
+{ config, lib, pkgs, namespace, ... }:
 let
   inherit (lib) mkEnableOption mkIf;
 
   cfg = config.${namespace}.programs.cli.bun;
-in
-{
+  bunInstallDir = "${config.${namespace}.user.home}/.bun";
+in {
   options.${namespace}.programs.cli.bun = {
     enable = mkEnableOption "Whether to enable Bun.";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      bun
-    ];
+    environment.systemPackages = with pkgs; [ bun ];
+
+    environment.variables = { BUN_INSTALL = bunInstallDir; };
+
+    environment.systemPath = [ "${bunInstallDir}/bin" ];
   };
 }
