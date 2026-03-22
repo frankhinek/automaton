81d0c5bcce6b21e1213b69e90842517a951c9d51
Frank Hinek <frankhinek@users.noreply.github.com>
2026-03-22 16:08:37 -0400

    chore(codex): bump package to v0.116.0

--- packages/codex/default.nix
+++ packages/codex/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenvNoCC, fetchurl, makeWrapper, installShellFiles }:
 
 let
-  version = "0.114.0";
+  version = "0.116.0";
 
   platform = if stdenvNoCC.hostPlatform.isDarwin
   && stdenvNoCC.hostPlatform.isAarch64 then
@@ -11,7 +11,7 @@ let
 
   sourceHashes = {
     "aarch64-apple-darwin" =
-      "sha256-yY61UGlfmersJ9+ZcaG3aoOssV61VSI4P6MbBJcpfFQ=";
+      "sha256-FIc19fIgmy9qMb18o2icYowubFJo/7oL6T93IPz4kvU=";
   };
 in stdenvNoCC.mkDerivation {
   pname = "codex";
