9ac55502eab2a2615ceb3006a0246c48424754fb
Frank Hinek <frankhinek@users.noreply.github.com>
2026-05-21 20:19:26 -0400

    chore: update OpenAI Codex to 0.133.0

--- packages/codex/default.nix
+++ packages/codex/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenvNoCC, fetchurl, makeWrapper, installShellFiles }:
 
 let
-  version = "0.130.0";
+  version = "0.133.0";
 
   platform = if stdenvNoCC.hostPlatform.isDarwin
   && stdenvNoCC.hostPlatform.isAarch64 then
@@ -11,7 +11,7 @@ let
 
   sourceHashes = {
     "aarch64-apple-darwin" =
-      "sha256-vFCkt/mgyMqZF5GJ5GWbYBEHgwdw4hVH3Awka85zNXc=";
+      "sha256-E8eDYr7/gUt93hbtdWN2I2d3Zphp3eRL0jJnNQDfaFM=";
   };
 in stdenvNoCC.mkDerivation {
   pname = "codex";
