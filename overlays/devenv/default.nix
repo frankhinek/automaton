5c13fe4d17d5db5314cb9e08b7c46717bcb62ba8
Frank Hinek <frankhinek@users.noreply.github.com>
2026-05-25 13:41:05 -0400

    chore(devenv): bump package to v2.1.2

--- overlays/devenv/default.nix
+++ overlays/devenv/default.nix
@@ -1,4 +1,4 @@
 { devenv, ... }:
 _final: prev: {
-  devenv_2_1 = devenv.packages.${prev.stdenv.hostPlatform.system}.devenv;
+  devenv_2_1_2 = devenv.packages.${prev.stdenv.hostPlatform.system}.devenv;
 }
