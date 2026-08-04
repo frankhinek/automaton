b66b46bdc181d50fa0354545abe6141da1968559
Frank Hinek <frankhinek@users.noreply.github.com>
2026-08-04 19:17:20 -0400

    chore(devenv): bump package to v2.2.1

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Pinned devenv release
-    devenv.url = "github:cachix/devenv/v2.1.2";
+    devenv.url = "github:cachix/devenv/v2.2.1";
 
     # Pinned Rust toolchains via rustup channels
     rust-overlay.url = "github:oxalica/rust-overlay";
--- modules/home/programs/cli/tools/devenv/default.nix
+++ modules/home/programs/cli/tools/devenv/default.nix
@@ -10,5 +10,5 @@ in {
   };
 
   config =
-    mkIf cfg.enable { home = { packages = with pkgs; [ devenv_2_1_2 ]; }; };
+    mkIf cfg.enable { home = { packages = with pkgs; [ devenv_2_2_1 ]; }; };
 }
