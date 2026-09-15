cef5ff22d24bcab81f8dfa93276646ef3c108ea8
Frank Hinek <frankhinek@users.noreply.github.com>
2026-09-15 15:03:29 -0400

    chore(devenv): bump package to v2.3.1

--- modules/home/programs/cli/tools/devenv/default.nix
+++ modules/home/programs/cli/tools/devenv/default.nix
@@ -10,5 +10,5 @@ in {
   };
 
   config =
-    mkIf cfg.enable { home = { packages = with pkgs; [ devenv_2_2_2 ]; }; };
+    mkIf cfg.enable { home = { packages = with pkgs; [ devenv_2_3_1 ]; }; };
 }
